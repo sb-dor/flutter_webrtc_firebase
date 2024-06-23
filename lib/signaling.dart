2f07f00f0214cbbb9a7bc899a2735a6368772572
sb-dor <avaz.shams.2002@gmail.com>
2024-06-23 14:43:55 +0500

    uploaded

--- lib/signaling.dart
+++ lib/signaling.dart
@@ -18,7 +18,10 @@ class Signaling {
   Map<String, dynamic> configuration = {
     'iceServers': [
       {
-        'urls': ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
+        'urls': [
+          'stun:stun1.l.google.com:19302',
+          'stun:stun2.l.google.com:19302',
+        ]
       }
     ]
   };
